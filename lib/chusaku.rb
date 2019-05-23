bbb383e8e8e0abebd1b2069838d21f4f587273f0
Nishiki Liu <nshki@users.noreply.github.com>
2019-05-23 19:00:33 -0400

    Add rudimentary routes parsing method

--- lib/chusaku.rb
+++ lib/chusaku.rb
@@ -3,4 +3,28 @@
 require 'chusaku/version'
 
 module Chusaku
+  class << self
+    def annotate
+      routes = Rails.application.routes.routes
+      actions = parse_data(routes)
+    end
+
+    protected
+
+      # Extract relevant information about a given set of routes.
+      #
+      # @param {ActionDispatch::Journey::Routes} routes
+      # @return {Hash}
+      def parse_data(routes)
+        routes.map do |route|
+          {
+            verb: route.verb,
+            path: route.path.spec.to_s,
+            name: route.name,
+            controller: route.default[:controller],
+            action: route.defaults[:action]
+          }
+        end
+      end
+  end
 end
