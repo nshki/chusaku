573d37c8b6e7a7d9ac08ea4847592188aa144f89
Nishiki Liu <nshki@users.noreply.github.com>
2019-05-23 22:48:19 -0400

    Move module methods out of class << self

--- lib/chusaku.rb
+++ lib/chusaku.rb
@@ -3,78 +3,81 @@
 require 'chusaku/version'
 
 module Chusaku
-  class << self
-    def annotate
-      routes = Rails.application.routes.routes
-      actions = parse_data(routes)
-      controller_paths = Dir.glob \
-        Rails.root.join('app/controllers/**/*_controller.rb')
+  # The main method to run Chusaku. Annotate all controller files in your Rails
+  # project as follows:
+  #
+  #   # @route GET /waterlilies/:id (waterlily)
+  #   def show
+  #     # ...
+  #   end
+  def self.annotate
+    routes = Rails.application.routes.routes
+    actions = parse_data(routes)
+    controller_paths = Dir.glob \
+      Rails.root.join('app/controllers/**/*_controller.rb')
 
-      puts 'Chusaku starting...'
+    puts 'Chusaku starting...'
 
-      # Loop over all controller files in the Rails project.
-      controller_paths.each do |path|
-        controller_key = /controllers\/(.*)_controller.rb/.match(path)[1]
-        controller_actions = actions[controller_key]
-        next if controller_actions.nil?
+    # Loop over all controller files in the Rails project.
+    controller_paths.each do |path|
+      controller_key = /controllers\/(.*)_controller.rb/.match(path)[1]
+      controller_actions = actions[controller_key]
+      next if controller_actions.nil?
 
-        # Annotate lines that contain an action definition.
-        File.open(path, 'r+') do |file|
-          lines = file.each_line.to_a
-          annotated_lines =
-            lines.map { |line| create_comment(line, controller_actions) }
-          file.rewind
-          file.write(annotated_lines.join)
-          puts "Annotated #{controller_key}"
-        end
+      # Annotate lines that contain an action definition.
+      File.open(path, 'r+') do |file|
+        lines = file.each_line.to_a
+        annotated_lines =
+          lines.map { |line| create_comment(line, controller_actions) }
+        file.rewind
+        file.write(annotated_lines.join)
+        puts "Annotated #{controller_key}"
       end
-
-      puts 'Chusaku finished!'
     end
 
-    protected
+    puts 'Chusaku finished!'
+  end
 
-      # Extract relevant information about a given set of routes.
-      #
-      # @param {ActionDispatch::Journey::Routes} routes
-      # @return {Hash}
-      def parse_data(routes)
-        data = {}
+  # Extract relevant information about a given set of routes.
+  #
+  # @param {ActionDispatch::Journey::Routes} routes
+  # @return {Hash}
+  def self.parse_data(routes)
+    data = {}
 
-        routes.each do |route|
-          action = route.defaults[:action]
-          data[route.defaults[:controller]] ||= {}
-          data[route.defaults[:controller]][action] =
-            {
-              verb: route.verb,
-              path: route.path.spec.to_s,
-              name: route.name
-            }
-        end
+    routes.each do |route|
+      action = route.defaults[:action]
+      data[route.defaults[:controller]] ||= {}
+      data[route.defaults[:controller]][action] =
+        {
+          verb: route.verb,
+          path: route.path.spec.to_s,
+          name: route.name
+        }
+    end
 
-        data
-      end
+    data
+  end
 
-      # Given a line, return a string that represents what should be written in
-      # place of that line with a route annotation.
-      #
-      # @param {String} line
-      # @param {Hash} actions
-      # @return {String}
-      def create_comment(line, actions)
-        match = /^(\s*)def\s+(.*)\s*$/.match(line)
-        return line if match.nil?
+  # Given a line, return a string that represents what should be written in
+  # place of that line with a route annotation.
+  #
+  # @param {String} line
+  # @param {Hash} actions
+  # @return {String}
+  def self.create_comment(line, actions)
+    match = /^(\s*)def\s+(.*)\s*$/.match(line)
+    return line if match.nil?
 
-        whitespace = match[1]
-        action = match[2]
-        return line if actions[action].nil?
+    whitespace = match[1]
+    action = match[2]
+    return line if actions[action].nil?
 
-        verb = actions[action][:verb]
-        path = actions[action][:path]
-        name = actions[action][:name]
-        comment = "#{whitespace}# @route #{verb} #{path} (#{name})\n"
+    verb = actions[action][:verb]
+    path = actions[action][:path]
+    name = actions[action][:name]
+    comment = "#{whitespace}# @route #{verb} #{path} (#{name})\n"
 
-        "#{comment}#{line}"
-      end
+    "#{comment}#{line}"
   end
 end
