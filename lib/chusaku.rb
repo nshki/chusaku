4da1a9449e96de1f90643815871d19fac4206399
Nishiki Liu <nshki@users.noreply.github.com>
2019-05-23 21:29:54 -0400

    Write first rudimentary draft

--- lib/chusaku.rb
+++ lib/chusaku.rb
@@ -7,6 +7,29 @@ module Chusaku
     def annotate
       routes = Rails.application.routes.routes
       actions = parse_data(routes)
+      controller_paths = Dir.glob \
+        Rails.root.join('app/controllers/**/*_controller.rb')
+
+      puts 'Chusaku starting...'
+
+      # Loop over all controller files in the Rails project.
+      controller_paths.each do |path|
+        controller_key = /controllers\/(.*)_controller.rb/.match(path)[1]
+        controller_actions = actions[controller_key]
+        next if controller_actions.nil?
+
+        # Annotate lines that contain an action definition.
+        File.open(path, 'r+') do |file|
+          lines = file.each_line.to_a
+          annotated_lines =
+            lines.map { |line| create_comment(line, controller_actions) }
+          file.rewind
+          file.write(annotated_lines.join)
+          puts "Annotated #{controller_key}"
+        end
+      end
+
+      puts 'Chusaku finished!'
     end
 
     protected
@@ -16,15 +39,42 @@ module Chusaku
       # @param {ActionDispatch::Journey::Routes} routes
       # @return {Hash}
       def parse_data(routes)
-        routes.map do |route|
-          {
-            verb: route.verb,
-            path: route.path.spec.to_s,
-            name: route.name,
-            controller: route.default[:controller],
-            action: route.defaults[:action]
-          }
+        data = {}
+
+        routes.each do |route|
+          action = route.defaults[:action]
+          data[route.defaults[:controller]] ||= {}
+          data[route.defaults[:controller]][action] =
+            {
+              verb: route.verb,
+              path: route.path.spec.to_s,
+              name: route.name
+            }
         end
+
+        data
+      end
+
+      # Given a line, return a string that represents what should be written in
+      # place of that line with a route annotation.
+      #
+      # @param {String} line
+      # @param {Hash} actions
+      # @return {String}
+      def create_comment(line, actions)
+        match = /^(\s*)def\s+(.*)\s*$/.match(line)
+        return line if match.nil?
+
+        whitespace = match[1]
+        action = match[2]
+        return line if actions[action].nil?
+
+        verb = actions[action][:verb]
+        path = actions[action][:path]
+        name = actions[action][:name]
+        comment = "#{whitespace}# @route #{verb} #{path} (#{name})\n"
+
+        "#{comment}#{line}"
       end
   end
 end
