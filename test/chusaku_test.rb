907b41458d6d800b8cda2a4c4f3a506c3c15d8cf
Nishiki Liu <nshki@users.noreply.github.com>
2019-05-24 00:26:28 -0400

    Add tests for .parse_data and .create_comment

--- test/chusaku_test.rb
+++ test/chusaku_test.rb
@@ -3,4 +3,67 @@
 require 'test_helper'
 
 class ChusakuTest < Minitest::Test
+  def test_parse_data
+    # Mock ActionDispatch::Journey::Routes objects.
+    routes = []
+    path1 = Minitest::Mock.new
+    path1.expect(:spec, 'path1')
+    path2 = Minitest::Mock.new
+    path2.expect(:spec, 'path2')
+    route1 = Minitest::Mock.new
+    route1.expect(:defaults, controller: 'controller1', action: 'action1')
+    route1.expect(:verb, 'GET')
+    route1.expect(:path, path1)
+    route1.expect(:name, 'name1')
+    routes.push(route1)
+    route2 = Minitest::Mock.new
+    route2.expect(:defaults, controller: 'controller2', action: 'action2')
+    route2.expect(:verb, 'POST')
+    route2.expect(:path, path2)
+    route2.expect(:name, 'name2')
+    routes.push(route2)
+
+    result = Chusaku.parse_data(routes)
+
+    assert_equal \
+      result,
+      'controller1' => {
+        'action1' => { verb: 'GET', path: 'path1', name: 'name1' }
+      },
+      'controller2' => {
+        'action2' => { verb: 'POST', path: 'path2', name: 'name2' }
+      }
+  end
+
+  def test_create_comment
+    actions =
+      {
+        'my_method' => { verb: 'GET', path: 'path1', name: 'name1' }
+      }
+
+    # Valid action.
+    line = '  def my_method'
+    result = Chusaku.create_comment(line, actions)
+    assert_equal("  # @route GET path1 (name1)\n  def my_method", result)
+
+    # Non-method line.
+    line = '  raise Exception'
+    result = Chusaku.create_comment(line, actions)
+    assert_equal('  raise Exception', result)
+
+    # Non-existent action.
+    line = 'def test'
+    result = Chusaku.create_comment(line, actions)
+    assert_equal('def test', result)
+
+    # Commented out action.
+    line = '# def my_method'
+    result = Chusaku.create_comment(line, actions)
+    assert_equal('# def my_method', result)
+
+    # Action with tabs.
+    line = '	def my_method'
+    result = Chusaku.create_comment(line, actions)
+    assert_equal("	# @route GET path1 (name1)\n	def my_method", result)
+  end
 end
