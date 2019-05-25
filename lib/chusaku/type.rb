14e77dfa2f04cf63956cdb7cdec50131ff470701
Nishiki Liu <nshki@users.noreply.github.com>
2019-05-24 21:57:42 -0400

    Modify Chusaku::Type to accept actions

--- lib/chusaku/type.rb
+++ lib/chusaku/type.rb
@@ -2,20 +2,23 @@
 
 module Chusaku
   module Type
-    # Given a line and an action, returns the line's type:
+    # Given a line and actions, returns the line's type:
     #
     #   1. comment - A line that is entirely commented. Lines that have trailing
     #                comments do not fall under this category.
-    #   2. action  - A line that contains the action definition.
+    #   2. action  - A line that contains an action definition.
     #   3. code    - Anything else.
     #
     # @param {String} line
-    # @param {String} action
+    # @param {Array<String>} actions
     # @return {Symbol}
-    def self.call(line:, action:)
-      if !/^(\s*)#.*$/.match(line).nil?
+    def self.call(line:, actions:)
+      comment_match = /^\s*#.*$/.match(line)
+      def_match = /^\s*def\s+(\w*)\s*\w*.*$/.match(line)
+
+      if !comment_match.nil?
         :comment
-      elsif !/^(\s*)def\s+#{action}\s*(\w*).*$/.match(line).nil?
+      elsif !def_match.nil? && actions.include?(def_match[1])
         :action
       else
         :code
--- test/type/type_test.rb
+++ test/type/type_test.rb
@@ -5,61 +5,61 @@ require 'test_helper'
 class TypeTest < Minitest::Test
   def test_comment
     line = '# foobar'
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:comment, result)
   end
 
   def test_comment_with_spaces
     line = '  # foobar  '
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:comment, result)
   end
 
   def test_comment_with_tabs
     line = '	# foobar	'
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:comment, result)
   end
 
   def test_comment_with_spaces_and_tabs
     line = '  	# foobar	  '
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:comment, result)
   end
 
   def test_action
-    line = 'def foobar'
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    line = 'def foo'
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:action, result)
   end
 
   def test_action_with_spaces
-    line = '  def foobar  '
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    line = '  def bar  '
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:action, result)
   end
 
   def test_action_with_tabs
-    line = '	def foobar	'
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    line = '	def foo	'
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:action, result)
   end
 
   def test_action_with_comment
-    line = 'def foobar # comment'
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    line = 'def bar # comment'
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:action, result)
   end
 
   def test_code
     line = 'puts "hello world!"'
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:code, result)
   end
 
   def test_code_with_comment
     line = 'puts "hello world!" # hey'
-    result = Chusaku::Type.call(line: line, action: 'foobar')
+    result = Chusaku::Type.call(line: line, actions: %w[foo bar])
     assert_equal(:code, result)
   end
 end
