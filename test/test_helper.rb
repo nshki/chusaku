cb25a3c75e6c549251e6f9a5532678b5d2b91f1a
Nishiki Liu <nshki@users.noreply.github.com>
2019-05-24 22:33:00 -0400

    Absorb Chusaku::Type into Chusaku::Parser

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'chusaku/type'
 require 'chusaku/parser'
 require 'chusaku/routes'
 require 'chusaku'
