09cdc468934bd1bf0eb1719ad2a862c020964474
Nishiki Liu <nshki@users.noreply.github.com>
2019-05-24 23:04:58 -0400

    Require mock files in test_helper

--- test/routes/routes_test.rb
+++ test/routes/routes_test.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'mock/rails'
 
 class RoutesTest < Minitest::Test
   def test_mock_rails
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+require 'mock/rails'
+require 'mock/file'
 require 'chusaku/parser'
 require 'chusaku/routes'
 require 'chusaku'
-
 require 'minitest/autorun'
