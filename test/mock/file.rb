baa9291b88f7a8edbcdd08e6199c5786aac74732
Nishiki Liu <nshki@users.noreply.github.com>
2019-10-11 12:02:13 -0400

    Fix up comment docs in mock File

--- test/mock/file.rb
+++ test/mock/file.rb
@@ -3,18 +3,19 @@
 class File
   @@written_files = {}
 
-  # Given a string, append it to a variable.
+  # Given a file's content and path, write the content in memory rather than in
+  # the actual file.
   #
-  # @param {String} str
-  # @param {String} path
+  # @param {String} content - Contents of file
+  # @param {String} path - File path
   # @return {void}
-  def test_write(str, path)
-    @@written_files[path] = str
+  def test_write(content, path)
+    @@written_files[path] = content
   end
 
-  # Get class variable value and clear.
+  # Get the content of files in memory.
   #
-  # @return {Array<String>}
+  # @return {Array<String>} List of files and their contents
   def self.written_files
     written_files = @@written_files
     @@written_files = []
