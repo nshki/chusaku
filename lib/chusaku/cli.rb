1446354971f8bfd2e41d8eead1d9c6601b0a82fc
Nishiki Liu <nshki@users.noreply.github.com>
2019-10-27 08:26:07 -0700

    Add comments to Chusaku::CLI

--- lib/chusaku/cli.rb
+++ lib/chusaku/cli.rb
@@ -13,15 +13,20 @@ module Chusaku
     Finished = Class.new(RuntimeError)
     NotARailsProject = Class.new(RuntimeError)
 
+    # Initializes a new instance of Chusaku::CLI.
+    #
+    # @return {Chusaku::CLI} Instance of this class
     def initialize
       @options = {}
     end
 
+    # Parse CLI flags, if any, and handle applicable behaviors.
+    #
+    # @param {Array<String>} args - CLI arguments
+    # @return {Integer} 0 on success, 1 on error
     def call(args = ARGV)
       optparser.parse!(args)
-
       check_for_rails_project
-
       Chusaku.call(options)
     rescue NotARailsProject
       warn('Please run chusaku from the root of your project.')
@@ -32,30 +37,54 @@ module Chusaku
 
     private
 
-    def check_for_rails_project
-      unless File.directory?('./app/controllers') && File.exist?('./Rakefile')
-        raise NotARailsProject
+      # Raises exception if Rails project cannot be detected.
+      #
+      # @raise {Chusaku::CLI::NotARailsProject} Exception if not Rails project
+      def check_for_rails_project
+        unless File.directory?('./app/controllers') && File.exist?('./Rakefile')
+          raise NotARailsProject
+        end
       end
-    end
 
-    def optparser
-      OptionParser.new do |opts|
-        opts.banner = 'Usage: chusaku [options]'
-        opts.on('--exit-with-error-on-annotation', 'Fail if any file was annotated') do |arg|
-          @options[:error_on_annotation] = true
-        end
-        opts.on('--dry-run', 'Run without file modifications') do |arg|
-          @options[:dry] = true
-        end
-        opts.on('-v', '--version', 'Show Chusaku version number and quit') do |arg|
-          puts(Chusaku::VERSION)
-          raise Finished
-        end
-        opts.on('-h', '--help', 'Show this help message and quit') do
-          puts(opts)
-          raise Finished
+      # Returns an instance of OptionParser with supported flags.
+      #
+      # @return {OptionParser} Preconfigured OptionParser instance
+      def optparser
+        OptionParser.new do |opts|
+          opts.banner = 'Usage: chusaku [options]'
+
+          opts.on(
+            '--exit-with-error-on-annotation',
+            'Fail if any file was annotated'
+          ) do
+            @options[:error_on_annotation] = true
+          end
+
+          opts.on(
+            '--dry-run',
+            'Run without file modifications'
+          ) do
+            @options[:dry] = true
+          end
+
+          opts.on(
+            '-v',
+            '--version',
+            'Show Chusaku version number and quit'
+          ) do
+            puts(Chusaku::VERSION)
+            raise Finished
+          end
+
+          opts.on(
+            '-h',
+            '--help',
+            'Show this help message and quit'
+          ) do
+            puts(opts)
+            raise Finished
+          end
         end
       end
-    end
   end
 end
