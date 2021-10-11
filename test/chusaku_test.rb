f6254669b93690fd5a3ffd1e5980dea2e9c219ff
Nishiki Liu <hello@nshki.com>
2021-10-10 18:18:42 -0700

    Print verbose changes with flag

--- test/chusaku_test.rb
+++ test/chusaku_test.rb
@@ -11,7 +11,7 @@ class ChusakuTest < Minitest::Test
 
     assert_equal(0, exit_code)
     assert_empty(File.written_files)
-    assert_includes(out, 'would be annotated')
+    assert_includes(out, 'This was a dry run so no files were changed.')
   end
 
   def test_exit_with_error_on_annotation_flag
@@ -22,7 +22,7 @@ class ChusakuTest < Minitest::Test
 
     assert_equal(1, exit_code)
     assert_equal(2, File.written_files.count)
-    assert_includes(out, 'status code 1')
+    assert_includes(out, 'Exited with status code 1.')
   end
 
   def test_dry_run_and_exit_with_error_flag
@@ -33,21 +33,40 @@ class ChusakuTest < Minitest::Test
 
     assert_equal(1, exit_code)
     assert_empty(File.written_files)
-    assert_includes(out, 'Annotations missing')
-    assert_includes(out, 'status code 1')
+    assert_includes(out, 'This was a dry run so no files were changed.')
+    assert_includes(out, 'Exited with status code 1.')
+  end
+
+  def test_verbose_flag
+    File.reset_mock
+    exit_code = 0
+
+    out, _err = capture_io { exit_code = Chusaku.call(verbose: true) }
+
+    assert_equal(0, exit_code)
+    assert_includes \
+      out,
+      <<~CHANGES_COPY
+        [test/mock/app/controllers/api/tacos_controller.rb:4]
+        ---OLD
+          def show; end
+        ---NEW
+          # @route GET / (root)
+          # @route GET /api/tacos/:id (taco)
+          def show; end
+      CHANGES_COPY
   end
 
   def test_mock_app
     File.reset_mock
     exit_code = 0
 
-    out, _err = capture_io { exit_code = Chusaku.call }
+    capture_io { exit_code = Chusaku.call }
     files = File.written_files
     base_path = 'test/mock/app/controllers'
 
     assert_equal(0, exit_code)
     assert(2, files.count)
-    assert_includes(out, 'Annotated')
     refute_includes(files, "#{base_path}/api/burritos_controller.rb")
 
     expected =
