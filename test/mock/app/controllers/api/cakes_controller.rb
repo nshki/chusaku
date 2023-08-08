c3e9b3e6ba0a5f407fb1e1e977c9b0a584281268
Nishiki Liu <hello@nshki.com>
2023-08-07 19:25:08 -0700

    Adjust tests to account for indirectly named routes
    This adds a `CakesController` in the test suite that has an `inherit`
    action that is named on its PUT route but not its GET path. The GET path
    should still get annotated with a name.

---
+++ test/mock/app/controllers/api/cakes_controller.rb
@@ -0,0 +1,6 @@
+class CakesController < ApplicationController
+  # This route's GET action should be named the same as its PUT action,
+  # even though only the PUT action is named.
+  def inherit
+  end
+end
